184bc2862ae2ced32b7d5cb1b5e54094222100e0
Arfino Restu Alam <arfino.alam@gmail.com>
2024-02-12 10:59:06 +0700

    Initial Commit

---
+++ weebs/weebsApp.swift
@@ -0,0 +1,17 @@
+//
+//  weebsApp.swift
+//  weebs
+//
+//  Created by Arfino Restu Alam on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct weebsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
